4b754372dda42204c092bac822cd0c4bfab52a90
Luana <luana_limas@hotmail.com>
2020-01-30 12:35:13 -0300

    Adicionando firebase

---
+++ js/init.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAATOmk1jtfft0vbHYZUtKEIFQCWV69lrk",
+    authDomain: "overwatch2-c162f.firebaseapp.com",
+    databaseURL: "https://overwatch2-c162f.firebaseio.com",
+    projectId: "overwatch2-c162f",
+    storageBucket: "overwatch2-c162f.appspot.com",
+    messagingSenderId: "849921210701",
+    appId: "1:849921210701:web:6295e9b9fd7dbd0a3d5f04"
+  };
+  // Initialize Firebase
+  firebase.inicializeapp(firebaseConfig);
